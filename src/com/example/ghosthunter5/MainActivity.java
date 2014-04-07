f20fe928d68137e104be93459a4f5849c99d9e4b
kkorupolu <kvk5wh@virginia.edu>
2014-04-07 16:06:03 -0400

    Map with getMyLocation() as functionality

--- src/com/example/ghosthunter5/MainActivity.java
+++ src/com/example/ghosthunter5/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ghosthunter5;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
 import com.google.android.gms.maps.GoogleMap;
@@ -10,22 +11,23 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends android.support.v4.app.FragmentActivity 
-implements OnMapClickListener {
+public class MainActivity extends FragmentActivity implements
+		OnMapClickListener {
 
 	private GoogleMap mMap;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		// Try to obtain the map from the SupportMapFragment.
-        mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                .getMap();
-        mMap.setOnMapClickListener(this);
-	}
 
+		// Try to obtain the map from the SupportMapFragment.
+		mMap = ((SupportMapFragment) getSupportFragmentManager()
+				.findFragmentById(R.id.map)).getMap();
+		mMap.setOnMapClickListener(this);
+		mMap.setMyLocationEnabled(true);
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -35,11 +37,10 @@ implements OnMapClickListener {
 
 	@Override
 	public void onMapClick(LatLng position) {
-		
-		mMap.addMarker(new MarkerOptions()
-							.position(position).
-							icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher)));
-		
+
+		mMap.addMarker(new MarkerOptions().position(position).icon(
+				BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher)));
+
 	}
 
 }
